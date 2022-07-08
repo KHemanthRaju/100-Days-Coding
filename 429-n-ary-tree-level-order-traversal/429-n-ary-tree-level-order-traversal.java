908e9c5d0107f2018ab061c769d98ea1ad8a9251
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2022-07-08 23:29:31 +0530

    Time: 4 ms (60.49%), Space: 47.2 MB (61.36%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            List<Integer> res = new ArrayList<>();
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                Node q1 = queue.poll();
+                res.add(q1.val);
+                queue.addAll(q1.children);
+            }
+            ans.add(res);
+			// List<Integer> current = new ArrayList<>(); // inorder to store the current level items
+			// int size = queue.size();
+			// for (int i=0; i<size; i++) {
+			// 	Node temp = queue.poll(); // pop out the first element from the queue.
+			// 	current.add(temp.val);   // add the element into current list.
+			// 	queue.addAll(temp.children); // add all the children of the node to the queue.
+			// }
+			// ans.add(current);
+        }
+        return ans;
+    }
+}
