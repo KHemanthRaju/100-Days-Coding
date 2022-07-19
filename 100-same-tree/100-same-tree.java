e436ea305a3148a2eb009257e4296cee431c2353
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2022-07-19 20:54:44 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (41.10%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,20 +15,8 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(p);
-        queue.add(q);
-        while(!queue.isEmpty()){
-            TreeNode q2 = queue.poll();
-            TreeNode q1 = queue.poll();
-            if(q1==null && q2==null) continue;
-            if(q1==null || q2==null) return false;
-            if(q1.val!=q2.val) return false;
-            queue.add(q1.left);
-            queue.add(q2.left);
-            queue.add(q1.right);
-            queue.add(q2.right);
-        }
-        return true;
+        if(p==null && q == null) return true;
+        if(p == null || q == null) return false;
+        return (p.val == q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
