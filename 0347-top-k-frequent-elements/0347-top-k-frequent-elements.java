267a26e26bd20e0d8e2963b67cef09da938f2ed5
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2023-02-16 00:27:41 +0530

    Time: 11 ms (73.05%), Space: 44.9 MB (55.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,18 +1,53 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i : nums){ map.put(i, map.getOrDefault(i, 0) + 1); }
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i:nums) map.put(i, map.getOrDefault(i,0)+1);
         
-        Queue<Integer> maxheap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
-        for(int key : map.keySet()){ 
-            maxheap.add(key); 
+        Queue<Integer> maxheap = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
+        for(int key:map.keySet()){
+            maxheap.add(key);
         }
-        
         int ans[] = new int[k];
-        for(int i = 0; i < k; i++){
-            ans[i] = maxheap.poll();
+        for(int i=0;i<k;i++){
+            ans[i]=maxheap.poll();
         }
-        
-        return ans; 
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Map<Integer, Integer> map = new HashMap<>();
+//         for(int i : nums){ map.put(i, map.getOrDefault(i, 0) + 1); }
+        
+//         Queue<Integer> maxheap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+//         for(int key : map.keySet()){ 
+//             maxheap.add(key); 
+//         }
+        
+//         int ans[] = new int[k];
+//         for(int i = 0; i < k; i++){
+//             ans[i] = maxheap.poll();
+//         }
+        
+//         return ans; 
