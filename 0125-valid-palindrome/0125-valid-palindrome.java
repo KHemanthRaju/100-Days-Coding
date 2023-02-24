d2ad1bc801540d7f27e5340a59f8e771c6ae530a
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2023-02-24 16:48:03 +0530

    Time: 740 ms (10.29%), Space: 43.6 MB (21.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String s1 = s.toLowerCase().trim();
-        String ans = "";
-        for(int i=0;i<s1.length();i++){
-            if(s1.charAt(i)>='a' && s1.charAt(i)<='z' || s1.charAt(i)>='0' && s1.charAt(i)<='9'){
-                ans+=s1.charAt(i);
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
             }
+            left++;
+            right--;
         }
-        StringBuilder s2 = new StringBuilder();
-        s2.append(ans);
-        String s3 = s2.reverse().toString();
-        if(ans.equals(s3)){
-            return true;
-        }
-        return false;
+        return true;
     }
 }
