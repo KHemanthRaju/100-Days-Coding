9f5d14fa992c7abab5e0fbdc33e6b4a5567e895a
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2023-03-22 08:24:40 +0530

    Time: 8 ms (46.27%), Space: 42.3 MB (48.59%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,31 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length()>s2.length() || s2.length()==0) return false;
-        if(s1.length()==0) return true;
-        
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
+        if(s1.length()==0){
+            return true;
+        }
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
         
         for(int i=0;i<s1.length();i++){
-            arr1[s1.charAt(i)-'a']++;
-            arr2[s2.charAt(i)-'a']++;
+            freq1[s1.charAt(i)-'a']++;
+            freq2[s2.charAt(i)-'a']++;
         }
         
         for(int i=0;i<s2.length()-s1.length();i++){
-            if(matches(arr1,arr2)){
+            if(match(freq1,freq2)){
                 return true;
             }
-            arr2[s2.charAt(i)-'a']--;
-            arr2[s2.charAt(i+s1.length())-'a']++;
+            freq2[s2.charAt(i+s1.length())-'a']++;
+            freq2[s2.charAt(i)-'a']--;
         }
-        if(matches(arr1,arr2)){
-            return true;
-        }
-        return false;
+        return match(freq1,freq2);
     }
-    public boolean matches(int[] arr1, int[] arr2){
-        for(int i=0;i<26;i++){
-            if(arr1[i]!=arr2[i]){
+    public boolean match(int[] freq1, int[] freq2){
+        for(int i=0;i<freq1.length;i++){
+            if(freq1[i]!=freq2[i]){
                 return false;
             }
         }
