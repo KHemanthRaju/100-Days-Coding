579f5c80321a9cdc2c5ab5058f012145d8d9258b
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2023-09-16 21:59:25 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (34.10%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.java
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -1,12 +1,19 @@
 class Solution {
     public int subtractProductAndSum(int n) {
-        char[] ch = (""+n).toCharArray();
         int prod = 1;
-        int sum = 0;
-        for(int i=0;i<ch.length;i++){
-            prod *= Integer.parseInt(String.valueOf(ch[i]));
-            sum += Integer.parseInt(String.valueOf(ch[i]));
+        int sum=0;
+        while(n!=0){
+            prod *= n%10;
+            sum += n%10;
+            n=n/10;
         }
+        // char[] ch = (""+n).toCharArray();
+        // int prod = 1;
+        // int sum = 0;
+        // for(int i=0;i<ch.length;i++){
+        //     prod *= Integer.parseInt(String.valueOf(ch[i]));
+        //     sum += Integer.parseInt(String.valueOf(ch[i]));
+        // }
         return prod - sum;
     }
 }
