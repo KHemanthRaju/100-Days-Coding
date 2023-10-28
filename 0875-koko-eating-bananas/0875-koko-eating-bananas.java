dda1a922f001a9cc70714a62cf9518a36f4ea796
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2023-10-28 08:03:22 +0530

    Time: 6 ms (99.46%), Space: 44 MB (62.54%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,15 +1,17 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int low = 0, high = (int)1e9;
-        while(low<high){
+        int res = -1;
+        while(low<=high){
             int mid = low + (high - low)/2;
             if(feasible(piles,mid,h)){
-                high = mid;
+                res = mid;
+                high = mid-1;
             }else{
                 low = mid + 1;
             }
         }
-        return low;
+        return res;
     }
     
     
