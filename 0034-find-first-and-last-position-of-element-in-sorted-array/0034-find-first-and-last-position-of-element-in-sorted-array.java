3cf6d779191b1416e65234c6ffeb545d159f243f
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2023-10-28 08:34:17 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (49.31%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int startIndex = -1, lastIndex= -1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                startIndex = i;
-                break;
+        int ans[] = new int[2];
+        ans[0] = leftBinarySearch(nums,target);
+        ans[1] = rightBinarySearch(nums,target);
+        return ans;
+    }
+    
+    public int leftBinarySearch(int[] nums, int target){
+        int res = -1;
+        int low = 0, high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                res = mid;
+                high = mid - 1;
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
-        for(int i=nums.length - 1; i>=0; i--){
-            if(nums[i] == target){
-                lastIndex = i;
-                break;
+        return res;
+    }
+    
+    public int rightBinarySearch(int[] nums, int target){
+        int res = -1;
+        int low = 0, high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                res = mid;
+                low = mid + 1;
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
-        
-        return new int[]{startIndex, lastIndex};
+        return res;
     }
 }
