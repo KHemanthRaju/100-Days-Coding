899891e3a3f790dadc06524efaaf1b243eb9ae24
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2023-11-11 08:13:35 +0530

    Time: 1018 ms (45.72%), Space: 54.4 MB (97.01%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.java
+++ 0336-palindrome-pairs/0336-palindrome-pairs.java
@@ -1,24 +1,38 @@
 class Solution {
     public List<List<Integer>> palindromePairs(String[] words) {
         List<List<Integer>> ans = new ArrayList<>();
+        if(words.length<2) return ans;
         HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < words.length; ++ i) map.put(words[i], i);
-    for (int i = 0; i < words.length; ++ i) {
-        int l = 0, r = 0;
-        while (l <= r) {
-            String s = words[i].substring(l, r);
-            Integer j = map.get(new StringBuilder(s).reverse().toString());
-            if (j != null && i != j && isPalindrome(words[i].substring(l == 0 ? r : 0, l == 0 ? words[i].length() : l)))
-                ans.add(Arrays.asList(l == 0 ? new Integer[]{i, j} : new Integer[]{j, i}));
-            if (r < words[i].length()) ++r;
-            else ++l;
+        for(int i=0;i<words.length;i++){
+            String reverse= new StringBuilder(words[i]).reverse().toString();
+            map.put(reverse,i);
         }
-    }
+        System.out.println(map);
+        for(int i=0;i<words.length;i++){
+            for(int j=0;j<=words[i].length();j++){
+                String s1 = words[i].substring(0,j);
+                String s2 = words[i].substring(j);
+                
+                if(map.containsKey(s1) && isPalindrome(s2) && map.get(s1)!=i){
+                    List<Integer> part1 = new ArrayList<>();
+                    part1.add(i);
+                    part1.add(map.get(s1));
+                    ans.add(part1);
+                }
+                if(!s1.isEmpty() && map.containsKey(s2) && isPalindrome(s1) && map.get(s2)!=i){
+                    List<Integer> part1 = new ArrayList<>();
+                    part1.add(map.get(s2));
+                    part1.add(i);
+                    ans.add(part1);
+                }
+            }
+        }
         return ans;
         
     }
     
     public boolean isPalindrome(String s){
+        // System.out.println(s);
         for(int i=0;i<s.length()/2;i++){
             if(s.charAt(i) != s.charAt(s.length()-1-i)){
                 return false;
