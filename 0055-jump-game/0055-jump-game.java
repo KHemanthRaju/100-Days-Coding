95376db4973e8fc4b5d40f78857a4661bd6474d9
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2023-12-09 15:26:42 +0530

    Time: 2 ms (81.66%), Space: 44.6 MB (30.78%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int far = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i>far) return false;
+            far = Math.max(far,i+nums[i]);
+        }
+        return true;
+    }
+}
