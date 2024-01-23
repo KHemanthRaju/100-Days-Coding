1cdef731870f618da90404e34d3afd675a4db69e
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2024-01-24 03:46:08 +0530

    Time: 22 ms (47.08%), Space: 44.7 MB (38.11%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -2,32 +2,33 @@ class Solution
 {
     public int maxLength(List<String> arr) 
     {
-        int max_len = 0;
-        
-        // [1] we should first throw away all strings with any 
-        //    duplicate characters; strings with all unique  
-        //    characters are the subsets of the alphabet, thus, 
-        //    can be encoded using binary form 
-        List<Integer> unique = new ArrayList<>();
-        for (String s : arr)
-        {
-            Integer bin = 0;
-            for (char ch : s.toCharArray()) bin += 1 << (ch - 'a');
-            if (Integer.bitCount(bin) == s.length()) unique.add(bin);
+        int[] max = new int[1];
+        backtrack(arr, 0, "", max);
+        return max[0];
+    }
+    
+    private static void backtrack(List<String> arr, int index, String current, int[] max) {
+        if (index == arr.size()) {
+            max[0] = Math.max(max[0], current.length());
+            return;
         }
 
-        // [2] now start with an empty concatenation and iteratively 
-        //    increase its length by trying to add more strings 
-        ArrayList<Integer> concat = new ArrayList<>(List.of(0));
-        for (Integer u : unique)
-            for (int i = concat.size() - 1; i >= 0; i--)
-                if ((concat.get(i) & u) == 0)
-                {
-                    Integer cc = concat.get(i) | u;
-                    concat.add(cc);
-                    max_len = Math.max(max_len, Integer.bitCount(cc));
-                }
+        // Include the current string
+        if (isUnique(current + arr.get(index))) {
+            backtrack(arr, index + 1, current + arr.get(index), max);
+        }
 
-        return max_len;
+        // Exclude the current string
+        backtrack(arr, index + 1, current, max);
+    }
+
+    private static boolean isUnique(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char ch : s.toCharArray()) {
+            if (!set.add(ch)) {
+                return false; // Character already present, not unique
+            }
+        }
+        return true; // All characters are unique
     }
 }
