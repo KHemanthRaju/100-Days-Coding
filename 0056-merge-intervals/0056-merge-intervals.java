47cecdab7fe45c2e64affe5f59a5c924b02df1b0
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2024-03-11 21:34:57 +0530

    Time: 8 ms (87.70%), Space: 46.2 MB (88.98%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,20 +1,14 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        //sort the array based on 0th index
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
-        //to store merged ans, create a linkedlist
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         LinkedList<int[]> merged = new LinkedList<>();
-        //for loop to iterate and compare each interval
         for(int[] interval:intervals){
-            //for non-overlapping condition
-            if(merged.isEmpty() || merged.getLast()[1] < interval[0]){
+            if(merged.isEmpty() || merged.getLast()[1]<interval[0]){
                 merged.add(interval);
             }else{
-                //for overlapping condition
                 merged.getLast()[1] = Math.max(merged.getLast()[1],interval[1]);
             }
         }
-        //convert linkedlist back to array and return it
         return merged.toArray(new int[merged.size()][]);
     }
 }
