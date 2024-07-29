9d9b13ef241f713d8a51ba7ab3b233d8525861f4
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2024-07-29 11:17:02 +0530

    Time: 4 ms (70.54%), Space: 44.5 MB (41.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,19 +3,32 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int arr[] = new int[26];
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-        }
-        for(int i=0;i<t.length();i++){
-            arr[t.charAt(i)-'a']--;
-        }
-        
-        for(int i:arr){
-            if(i!=0){
+        char[] ans1 = s.toCharArray();
+        char[] ans2 = t.toCharArray();
+        Arrays.sort(ans1);
+        Arrays.sort(ans2);
+        for(int i=0,j=0;i<s.length();i++,j++){
+            if(ans1[i]!=ans2[j]){
                 return false;
             }
         }
         return true;
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         int arr[] = new int[26];
+//         for(int i=0;i<s.length();i++){
+//             arr[s.charAt(i)-'a']++;
+//         }
+//         for(int i=0;i<t.length();i++){
+//             arr[t.charAt(i)-'a']--;
+//         }
+        
+//         for(int i:arr){
+//             if(i!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
     }
 }
