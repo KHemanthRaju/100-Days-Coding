2f978626ffeb434445c5d84cfa3791280b1b1c0a
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2024-09-19 18:58:34 -0700

    Time: 3 ms (82.90%) | Memory: 41 MB (99.89%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[0]=true;
+        int maxLen = 0;
+        for(String word : wordDict){
+            maxLen = Math.max(maxLen, word.length());
+        }
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<i;j++){
+                if(i-j>maxLen){
+                    continue;
+                }
+                if(dp[j] && wordDict.contains(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
