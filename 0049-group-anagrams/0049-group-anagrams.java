67761809fc6092a6a2b5e9981e1dac8e9386695a
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2024-10-06 13:24:37 -0700

    Time: 6 ms (97.82%), Space: 48 MB (33.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,27 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map = new HashMap<String,List<String>>();
-        for(String st: strs){
-            char[] ch = st.toCharArray();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] ch = s.toCharArray();
             Arrays.sort(ch);
             String keyStr = String.valueOf(ch);
             if(!map.containsKey(keyStr)){
-                map.put(keyStr,new ArrayList<>());
+                map.put(keyStr, new ArrayList<>());
             }
-            map.get(keyStr).add(st);
+            map.get(keyStr).add(s);
         }
         return new ArrayList<>(map.values());
     }
 }
-
-// HashMap<String,List<String>> map = new HashMap<String,List<String>>();
-//         for(String st:strs){
-//             char[] ch = st.toCharArray();
-//             Arrays.sort(ch);
-//             String keyStr = String.valueOf(ch);
-//             if(!map.containsKey(keyStr)){
-//                 map.put(keyStr,new ArrayList<>());
-//             }
-//             map.get(keyStr).add(st);
-//         }
-//         return new ArrayList<>(map.values());
