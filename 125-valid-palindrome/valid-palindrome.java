af8bbdb87f7cd67d9854d18da609a15539a36b5b
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2024-10-06 13:05:38 -0700

    Time: 13 ms (40.72%) | Memory: 44.6 MB (42.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,13 +3,14 @@ class Solution {
         s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
         int left = 0;
         int right = s.length()-1;
-        while(left<right){
+        while(left < right){
             if(s.charAt(left)!=s.charAt(right)){
                 return false;
             }
             left++;
             right--;
         }
+        
         return true;
     }
 }
