470e7a06a524c2f1d558ce95634319eed8e36086
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2024-10-08 16:57:13 -0700

    Time: 18 ms (80.69%) | Memory: 54.9 MB (96.47%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -13,13 +13,25 @@ class Solution {
 //             [0,3]
             
 //         {1,2,3}
-        for(int i=0;i<nums.length;i++){ //n
-            for(int j=i+1;j-i<=k && j<nums.length;j++){ //k value
-                 if(nums[i]==nums[j]){
-                     return true;
-                 }
+        
+//         for(int i=0;i<nums.length;i++){ //n
+//             for(int j=i+1;j-i<=k && j<nums.length;j++){ //k value
+//                  if(nums[i]==nums[j]){
+//                      return true;
+//                  }
                 
+//             }
+//         }
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){ //n
+            if(set.contains(nums[i])){
+                return true;
             }
+            set.add(nums[i]); // 1,2,3,1
+            if(set.size() > k){
+                set.remove(nums[i-k]);
+            }
+            
         }
         return false;
         
