780734350c27083101317c5195643b8c8fca55eb
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-01-20 18:28:46 -0700

    Time: 871 ms (69.11%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,18 @@
 # Write your MySQL query statement below
+-- With cte as
+-- (
+--     select a.product_id, price, units, price*units as sales
+--     from prices a
+--     left join unitssold b
+--     on a.product_id = b.product_id
+--     where b.purchase_date between a.start_date and a.end_date
+-- )
+
+-- select product_id, coalesce(round(sum(sales)/sum(units)*1.00,2),0) as average_price
+-- from cte
+-- group by product_id
+
+# Write your MySQL query statement below
 With cte as
 (
     select a.product_id,
