c4a293affea77fd833a0030147afc56010f13a8f
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-03-09 18:23:17 -0700

    Time: 134 ms (79.78%) | Memory: 41.6 MB (62.32%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -3,20 +3,19 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int rows = board.length;
         int cols = board[0].length;
-        
         visited = new boolean[rows][cols];
-        
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(word.charAt(0) == board[i][j] && searchWord(i, j, 0, word, board)){
+                if(word.charAt(0)==board[i][j] && searchWord(i,j,0,word,board)){
                     return true;
                 }
             }
         }
         return false;
+
     }
-    
-    public boolean searchWord(int i, int j, int index,String word, char[][] board){
+
+    public boolean searchWord(int i,int j,int index, String word, char[][] board){
         if(index==word.length()) return true;
         
         if(i<0 || i>=board.length || j<0 || j>=board[i].length || board[i][j] != word.charAt(index) || visited[i][j]) return false;
@@ -32,4 +31,6 @@ class Solution {
         visited[i][j]= false;
         return false;
     }
+
+
 }
