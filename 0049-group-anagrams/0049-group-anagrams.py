2ef318b4d09cc43ca1f5c46e6ce74f885e8736a0
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-05-02 15:45:46 -0700

    Time: 19 ms (80.99%), Space: 16.2 MB (65.04%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        adict = {}
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word not in adict:
+                adict[sorted_word] = []
+            adict[sorted_word].append(word)
+        return list(adict.values())
+        
