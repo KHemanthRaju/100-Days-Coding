c6c416c5670b795651d6ea78e0e738e37245fa88
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-05-13 13:07:28 -0700

    Time: 0 ms (100%), Space: 19.1 MB (11.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ans = []
         maps = {}
         for i in range(len(nums)):
             if target - nums[i] in maps:
