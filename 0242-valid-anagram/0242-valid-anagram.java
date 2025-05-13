cff39dc2ae463d457f5278030d9ee0ab1619c6ec
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-05-13 12:36:01 -0700

    Time: 3 ms (90.82%), Space: 44.7 MB (44.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-        char[] ans1 = s.toCharArray();
-        char[] ans2 = t.toCharArray();
-        Arrays.sort(ans1);
-        Arrays.sort(ans2);
-        for(int i=0,j=0;i<s.length();i++,j++){
-            if(ans1[i]!=ans2[j]){
+        if(s.length()!= t.length()) return false;
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i]!=ch2[i]){
                 return false;
             }
         }
         return true;
-//         if(s.length()!=t.length()){
-//             return false;
-//         }
-//         int arr[] = new int[26];
-//         for(int i=0;i<s.length();i++){
-//             arr[s.charAt(i)-'a']++;
-//         }
-//         for(int i=0;i<t.length();i++){
-//             arr[t.charAt(i)-'a']--;
-//         }
-        
-//         for(int i:arr){
-//             if(i!=0){
-//                 return false;
-//             }
-//         }
-//         return true;
     }
 }
