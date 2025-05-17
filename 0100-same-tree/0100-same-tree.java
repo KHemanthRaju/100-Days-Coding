3a8006442b5fe1620749941bb8ef63dce2fb2088
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-05-17 15:11:29 -0700

    Time: 0 ms (100%), Space: 41.5 MB (11.57%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,23 +15,16 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> q1 = new LinkedList<>();
-        Queue<TreeNode> q2 = new LinkedList<>();
-        q1.add(p);
-        q2.add(q);
-
-        while(!q1.isEmpty() && !q2.isEmpty()){
-            for(int i=0;i<q1.size();i++){
-            TreeNode node1 = q1.poll();
-            TreeNode node2 = q2.poll();
+        Stack<TreeNode[]> st = new Stack<>();
+        st.push(new TreeNode[]{p,q});
+        while(!st.isEmpty()){
+            TreeNode[] nodes = st.pop();
+            TreeNode node1 = nodes[0], node2 = nodes[1];
             if(node1==null && node2==null) continue;
-            if(node1==null || node2==null || node1.val != node2.val) return false;
-                q1.add(node1.left);
-                q2.add(node2.left);
+            if(node1==null || node2==null || node1.val!=node2.val) return false;
             
-                q1.add(node1.right);
-                q2.add(node2.right);
-            }
+            st.push(new TreeNode[]{node1.right, node2.right});
+            st.push(new TreeNode[]{node1.left, node2.left});
         }
         return true;
     }
