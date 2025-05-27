7f3a06b83cac643eb774dbb9e14a011cef80867c
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-05-27 16:56:52 -0700

    Time: 4 ms (69.37%), Space: 21.1 MB (80.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,13 +3,13 @@ class Solution:
         count = {}
         for num in nums:
             count[num] = 1 + count.get(num,0)
-
+        
         heap = []
         for num in count.keys():
-            heapq.heappush(heap, (count[num], num))
-            if len(heap) > k:
+            heapq.heappush(heap, (count[num],num))
+            if len(heap)>k:
                 heapq.heappop(heap)
-
+        
         ans = []
         for i in range(k):
             ans.append(heapq.heappop(heap)[1])
