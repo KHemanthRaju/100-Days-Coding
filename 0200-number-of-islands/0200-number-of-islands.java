7b3ebda8e4dd16e26bd7bbb29530526deb824bf4
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-07-05 15:24:16 -0700

    Time: 3 ms (83.87%), Space: 49.8 MB (40.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,31 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0].length==0){
+        if(grid == null || grid.length==0 || grid[0].length == 0){
             return 0;
         }
-        
-        int gridRows = grid.length;
-        int gridCols = grid[0].length;
-        int numIslands = 0;
-        
-        for(int i=0;i<gridRows;i++){
-            for(int j=0;j<gridCols;j++){
+
+        int islands = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == '1'){
-                    numIslands++;
                     dfs(grid, i, j);
+                    islands++;
                 }
             }
         }
-        return numIslands;
+
+        return islands;
     }
-    
-    public void dfs(char[][] grid, int i, int j){
-        int gridRows = grid.length;
-        int gridCols = grid[0].length;
-        
-        if(i<0 || i>=gridRows || j<0 || j>=gridCols || grid[i][j]=='0'){
+
+    public void dfs(char[][] grid, int row, int col){
+        if(row<0 || col<0 || row>=grid.length || col>=grid[0].length || grid[row][col]=='0') {
             return;
         }
-        
-        grid[i][j] = '0';
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+        grid[row][col] = '0';
+
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
     }
 }
