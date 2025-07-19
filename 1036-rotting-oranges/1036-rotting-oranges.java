30f203999685da27af7bca8e12ade63b798a4c66
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-07-19 16:40:30 -0700

    Time: 2 ms (67.94%), Space: 42.5 MB (49.46%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,37 +1,34 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid == null || grid.length == 0|| grid[0].length == 0){
-            return 0;
-        }
-
-        int fresh = 0;
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
         Queue<int[]> queue = new LinkedList<>();
+        int fresh = 0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
                     queue.offer(new int[]{i,j,0});
-                }else if(grid[i][j] == 1){
+                }
+                if(grid[i][j] == 1){
                     fresh++;
                 }
             }
         }
         int time = 0;
-        int dir[][] = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+        int[][] dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
         while(!queue.isEmpty()){
             int[] q = queue.poll();
             int r = q[0], c = q[1], t = q[2];
             time = t;
-            for(int[] dire: dir){
+            for(int[] dire:dir){
                 int nr = r + dire[0];
                 int nc = c + dire[1];
-
-                if(0<=nr && nr<grid.length && 0<=nc && nc<grid[0].length && grid[nr][nc]==1){
-                    grid[nr][nc]=2;
+                if(0<=nr && nr<grid.length && 0<=nc && nc<grid[0].length && grid[nr][nc] == 1){
                     fresh--;
-                    queue.offer(new int[]{nr,nc, t+1});
+                    grid[nr][nc] = 2;
+                    queue.offer(new int[]{nr,nc,t+1});
                 }
             }
         }
-        return fresh==0 ? time : -1;
+        return fresh == 0 ? time : -1;
     }
 }
