b96af858b3e376ee804da2c706b69adf4f0a9651
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-07-19 15:03:55 -0700

    Time: 1 ms (100%), Space: 44.4 MB (90.89%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,32 +1,27 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0].length == 0){
-            return 0;
-        }
         int maxArea = 0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==1){
-                    maxArea = Math.max(dfs(grid, i, j), maxArea);
+                    maxArea = Math.max(dfs(grid, i, j),maxArea);
+                    
                 }
             }
         }
         return maxArea;
     }
 
-    public int dfs(int[][] grid, int row, int col){
-        if(row < 0 || col < 0 || row>=grid.length || col >= grid[0].length || grid[row][col] == 0){
+    public int dfs(int[][] grid, int i, int j){
+        if(i<0 || i>= grid.length || j<0 || j>=grid[0].length || grid[i][j]==0){
             return 0;
         }
-
-        grid[row][col] = 0;
+        grid[i][j] = 0;
         int count = 1;
-        count += dfs(grid, row + 1, col);
-        count += dfs(grid, row - 1, col);
-        count += dfs(grid, row, col + 1);
-        count += dfs(grid, row, col - 1);
-
+        count += dfs(grid, i+1, j);
+        count += dfs(grid, i-1, j);
+        count += dfs(grid, i, j+1);
+        count += dfs(grid, i, j-1);
         return count;
     }
-
 }
