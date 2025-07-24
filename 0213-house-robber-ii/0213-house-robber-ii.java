0951cad3bd06b50fbad547bd98ae91545af3fa1c
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-07-24 16:50:11 -0700

    Time: 0 ms (100%), Space: 41.3 MB (37.68%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,37 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length<2){
-            return nums[0];
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int[] first_half = new int[n];
+        int[] last_half = new int[n];
+
+        for(int i=0;i<n-1;i++){
+            first_half[i] = nums[i];
+            last_half[i] = nums[i+1];
         }
-        
-        int[] total_loot_skip_last = new int[nums.length-1];
-        int[] total_loot_skip_first = new int[nums.length-1];
-        
-        for(int i=0;i<nums.length-1;i++){
-            total_loot_skip_last[i]=nums[i];
-            total_loot_skip_first[i] = nums[i+1];
-        }
-        
-        int ans1 = robHelper(total_loot_skip_last);
-        int ans2 = robHelper(total_loot_skip_first);
-        
-        return Math.max(ans1,ans2);
-        
+
+        return Math.max(robpart(first_half), robpart(last_half));
     }
-    
-    public int robHelper(int[] nums){
-        if(nums.length<2){
-            return nums[0];
-        }
-        
-        int[] dp = new int[nums.length];
-        dp[0]=nums[0];
+
+    public int robpart(int[] nums){
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int[] dp = new int[nums.length+1];
+        dp[0] = nums[0];
         dp[1] = Math.max(nums[0],nums[1]);
-        
-        for(int i=2;i<nums.length;i++){
-            dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
         }
-        
-        return dp[nums.length-1];
+        return dp[n-1];
     }
 }
