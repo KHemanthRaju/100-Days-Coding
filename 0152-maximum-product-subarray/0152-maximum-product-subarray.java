c1b3a3ec87f130aabb139c17dc54bedaa2721c2c
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-07-25 04:57:16 -0700

    Time: 2 ms (55.01%), Space: 45.4 MB (13.32%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,17 +1,15 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int ans=nums[0];
+        if(nums.length==1) return nums[0];
+        int maxResult = 0; 
         int l=1,r=1;
         for(int i=0;i<nums.length;i++){
-            
-            l = l==0 ? 1:l;
-            r = r==0 ? 1:r;
-            
+            l = (l==0)?1:l;
+            r = (r==0)?1:r;
             l*=nums[i];
             r*=nums[nums.length-1-i];
-            
-            ans = Math.max(ans,Math.max(l,r));
+            maxResult = Math.max(maxResult, Math.max(l,r));
         }
-        return ans;
+        return maxResult;
     }
 }
