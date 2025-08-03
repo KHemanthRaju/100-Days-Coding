abb3aea4317f7ee2162f5c59f56272bfbf09bd1e
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-08-03 10:56:08 -0700

    Time: 0 ms (100%), Space: 17.8 MB (65.47%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,21 +4,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
+Understand - Input - 2 trees, Output - boolean,  edge cases - 2 trees are empty-return true, 1 tree is empty, another tree has node - return false
+
+Plan - root1 and root2 is null, iterative, queue - [p.val, q.val] , if p.val==q.val, p.val != q.val, return false, [p.left, q.left], [p.right, q.right]
+"""
+
+from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        q1 = deque([p])
-        q2 = deque([q])
+        queue = deque([(p,q)])
 
-        while q1 and q2:
-            for _ in range(len(q1)):
-                node1 = q1.popleft()
-                node2 = q2.popleft()
-                if node1==None and node2==None:
-                    continue
-                if node1==None or node2==None or node1.val!=node2.val:
-                    return False
-                q1.append(node1.left)
-                q2.append(node2.left)
-                q1.append(node1.right)
-                q2.append(node2.right)
+        while queue:
+            node1, node2 = queue.popleft()
+
+            if not node1 and not node2:
+                continue
+
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+        
         return True
+        
