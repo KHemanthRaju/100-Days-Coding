4e2e2ff62a96a31510f16640abfd9a10d1c040d9
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-08-03 19:21:46 -0700

    Time: 8 ms (60.43%), Space: 42.8 MB (67.8%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length() || s2.length()==0) return false;
-        if(s1.length()==0) return true;
+        if(s1.length()>s2.length()) return false;
+
 
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
@@ -12,18 +12,16 @@ class Solution {
         }
 
         for(int i=0;i<s2.length()-s1.length();i++){
-            if(match(freq1,freq2)) return true;
+            if(match(freq1, freq2)) return true;
             freq2[s2.charAt(i+s1.length())-'a']++;
             freq2[s2.charAt(i)-'a']--;
         }
-        return match(freq1,freq2);
+        return match(freq1, freq2);
     }
 
     public boolean match(int[] freq1, int[] freq2){
         for(int i=0;i<freq1.length;i++){
-            if(freq1[i]!=freq2[i]){
-                return false;
-            }
+            if(freq1[i]!=freq2[i]) return false;
         }
         return true;
     }
