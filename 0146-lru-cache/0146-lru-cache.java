1c51820823d2da03f65e23cbe9f23ce48cabeb71
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-08-04 04:16:07 -0700

    Time: 739 ms (5.43%), Space: 118.3 MB (19.85%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,44 @@
+class LRUCache {
+
+    private ArrayList<int[]> cache;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.cache = new ArrayList<>();
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        for(int i=0;i<cache.size();i++){
+            if(cache.get(i)[0] == key){
+                int[] temp = cache.remove(i);
+                cache.add(temp);
+                return temp[1];
+            }
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        for(int i=0;i<cache.size();i++){
+            if(cache.get(i)[0] == key){
+                int[] temp = cache.remove(i);
+                temp[1] = value;
+                cache.add(temp);
+
+                return;
+            }
+        }
+        if(capacity == cache.size()){
+            cache.remove(0);
+        }
+        cache.add(new int[]{key, value});
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
