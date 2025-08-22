0014177bbc7df28198d73422da3477250e7f83b0
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-08-21 20:12:45 -0700

    Time: 0 ms (100%), Space: 40.5 MB (60.76%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -3,10 +3,11 @@ class Solution {
         int[][] grid = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || j==0){
+                if(i==0||j==0){
                     grid[i][j] = 1;
-                }else{
-                    grid[i][j] = grid[i][j-1]+grid[i-1][j];
+                }
+                else{
+                    grid[i][j] = grid[i-1][j]+grid[i][j-1];
                 }
             }
         }
