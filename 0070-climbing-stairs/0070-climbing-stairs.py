151610f477a2170fbc5a2054a2cd9d02048d68e6
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-08-30 13:37:24 -0700

    Time: 0 ms (100%), Space: 12.3 MB (98.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n<=2:
-            return n
-        dp = [0]*(n+1)
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        dp = [0] * (n+1)
         dp[0] = 0
         dp[1] = 1
         dp[2] = 2
         for i in range(3,n+1):
             dp[i] = dp[i-1]+dp[i-2]
-        return dp[n]
-        
+        return dp[-1]
