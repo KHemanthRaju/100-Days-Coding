903660dbcd653ad8693ddd1bc40c44c1d4e8e8af
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-08-31 15:32:14 -0700

    Time: 95 ms (18.76%), Space: 32.1 MB (66.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,6 +3,6 @@ class Solution:
         maxCurrent = nums[0]
         maxLength = nums[0]
         for i in range(1,len(nums)):
-            maxCurrent = max(nums[i], maxCurrent + nums[i])
+            maxCurrent = max(nums[i], maxCurrent+nums[i])
             maxLength = max(maxLength, maxCurrent)
-        return maxLength 
+        return maxLength
