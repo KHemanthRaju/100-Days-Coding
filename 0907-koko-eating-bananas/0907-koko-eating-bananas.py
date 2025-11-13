fff9224f7035075ea18027a7cea5c0290de18fe7
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-11-13 04:53:54 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,7 +1,6 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         minSpeed = 1
-        
         maxSpeed = max(piles)
         res = maxSpeed
         def canTakeTime(piles, h, speed):
@@ -9,11 +8,11 @@ class Solution:
             for pile in piles:
                 hours += math.ceil(float(pile)/speed)
             return hours<=h
-        while minSpeed <= maxSpeed:
+        while minSpeed < maxSpeed:
             speed = (minSpeed+maxSpeed)//2
             if canTakeTime(piles, h, speed):
                 res = speed
-                maxSpeed = speed - 1
+                maxSpeed = speed-1
             else:
-                minSpeed = speed + 1
+                minSpeed = speed+1
         return res
