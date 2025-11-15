ea7b0aa77da20dc87dfa7e78719e0e7f55667169
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-11-15 12:01:42 -0700

    Time: 10 ms (14.68%), Space: 65.3 MB (11.39%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -5,13 +5,13 @@ class Solution {
         helper(0,s,path,ans);
         return ans;
     }
-    
+
     public void helper(int index, String s, List<String> path, List<List<String>> ans){
         if(index == s.length()){
             ans.add(new ArrayList<>(path));
             return;
         }
-        for(int i = index;i<s.length();i++){
+        for(int i=index;i<s.length();i++){
             if(isPalindrome(s,index,i)){
                 path.add(s.substring(index,i+1));
                 helper(i+1,s,path,ans);
@@ -19,10 +19,10 @@ class Solution {
             }
         }
     }
-    
+
     public boolean isPalindrome(String s, int start, int end){
         while(start<=end){
-            if(s.charAt(start++) != s.charAt(end--)){
+            if(s.charAt(start++)!=s.charAt(end--)){
                 return false;
             }
         }
