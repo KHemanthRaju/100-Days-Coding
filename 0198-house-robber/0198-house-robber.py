53c891895174018cd427bcb5c0a521a393842425
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-11-16 14:36:40 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,10 +4,10 @@ class Solution:
         dp = [0] * (n+1)
         if n==1:
             return nums[0]
-        if n==2:
+        if n == 2:
             return max(nums[0], nums[1])
         dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        dp[1] = nums[1]
         for i in range(2, n):
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
         return dp[n-1]
