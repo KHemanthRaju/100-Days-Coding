745518537dd2cecf2b5aa206b29dfff4e6137cb7
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-11-30 13:50:52 -0700

    Time: 759 ms (5%), Space: 130 MB (48.72%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,5 +1,4 @@
 class LRUCache {
-
     ArrayList<int[]> cache;
     int capacity;
     public LRUCache(int capacity) {
@@ -22,7 +21,7 @@ class LRUCache {
     public void put(int key, int value) {
         for(int i=0;i<cache.size();i++){
             if(cache.get(i)[0] == key){
-                int temp[] = cache.remove(i);
+                int[] temp = cache.remove(i);
                 temp[1] = value;
                 cache.add(temp);
                 return;
