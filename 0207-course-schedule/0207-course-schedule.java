d33ed2b1b0af0a3846e78508a80b301fb30dae0a
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-03 04:21:06 -0700

    Time: 7 ms (54.38%), Space: 46.8 MB (45.3%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,32 +1,32 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> ans = new ArrayList<>();
+        int completed = 0;
+        int[] indegree = new int[numCourses];
         for(int i=0;i<numCourses;i++){
             ans.add(new ArrayList<>());
         }
-        int completed = 0;
-        int indegree[] = new int[numCourses];
         for(int[] pre:prerequisites){
             ans.get(pre[1]).add(pre[0]);
             indegree[pre[0]]++;
         }
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<numCourses;i++){
+        for(int i=0;i<indegree.length;i++){
             if(indegree[i]==0){
                 q.add(i);
             }
         }
-
         while(!q.isEmpty()){
-            int cool = q.poll();
+            int a = q.poll();
             completed++;
-            for(int num:ans.get(cool)){
+            for(int num:ans.get(a)){
                 indegree[num]--;
                 if(indegree[num]==0){
-                    q.add(num);
+                    q.offer(num);
                 }
             }
+
         }
-        return completed == numCourses;
+        return completed == numCourses?true:false;
     }
 }
