660a0285701c37b413e836bfaa16d39b729a30f4
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-03 01:41:27 -0700

    Time: 10 ms (31.29%), Space: 47.2 MB (7.49%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -2,30 +2,30 @@ class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int rows = heights.length;
         int cols = heights[0].length;
-        List<List<Integer>> ans = new ArrayList<>();
         Queue<int[]> pacificQueue = new LinkedList<>();
         Queue<int[]> atlanticQueue = new LinkedList<>();
         boolean[][] pacificVisited = new boolean[rows][cols];
         boolean[][] atlanticVisited = new boolean[rows][cols];
+        List<List<Integer>> ans = new ArrayList<>();
 
         for(int i=0;i<rows;i++){
             pacificQueue.offer(new int[]{i,0});
-            atlanticQueue.offer(new int[]{i,cols-1});
+            atlanticQueue.offer(new int[]{i, cols-1});
             pacificVisited[i][0] = true;
             atlanticVisited[i][cols-1] = true;
         }
-
         for(int i=0;i<cols;i++){
             pacificQueue.offer(new int[]{0,i});
             atlanticQueue.offer(new int[]{rows-1,i});
             pacificVisited[0][i] = true;
             atlanticVisited[rows-1][i] = true;
         }
+
         bfs(heights, pacificQueue, pacificVisited);
         bfs(heights, atlanticQueue, atlanticVisited);
-        for(int i=0;i<rows;i++){
+        for(int i=0;i<rows; i++){
             for(int j=0;j<cols;j++){
-                if(pacificVisited[i][j] && atlanticVisited[i][j]){
+                if(atlanticVisited[i][j] && pacificVisited[i][j]){
                     ans.add(Arrays.asList(i,j));
                 }
             }
@@ -33,24 +33,24 @@ class Solution {
         return ans;
     }
 
-    public void bfs(int[][] heights, Queue<int[]> queue, boolean[][] visited){
-        int directions[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        int rows = heights.length;
-        int cols = heights[0].length;
-        while(!queue.isEmpty()){
-            int[] q = queue.poll();
-            int r = q[0], c = q[1];
-            for(int dir[]:directions){
-                int nr = r + dir[0];
-                int nc = c + dir[1];
-                if(nr<0 || nr>=rows || nc<0 || nc>=cols || visited[nr][nc]==true){
+    public void bfs(int[][] heights, Queue<int[]> q, boolean[][] visited){
+        int[][] directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        int row = heights.length;
+        int col = heights[0].length;
+        while(!q.isEmpty()){
+            int[] ans = q.poll();
+            int r = ans[0], c = ans[1];
+            for(int[] dir:directions){
+                int r1 = dir[0]+r;
+                int c1 = dir[1] + c;
+                if(0>r1 || r1>=heights.length || 0>c1 || c1 >= heights[0].length || visited[r1][c1]==true){
                     continue;
                 }
-                if(heights[nr][nc]<heights[r][c]){
+                if(heights[r1][c1]<heights[r][c]){
                     continue;
                 }
-                visited[nr][nc]=true;
-                queue.offer(new int[]{nr,nc});
+                visited[r1][c1] = true;
+                q.offer(new int[]{r1,c1});
             }
         }
     }
