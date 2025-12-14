e224d072fc9683c80edad89b8b2334e935593aff
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-14 11:47:58 -0700

    Time: 83 ms (10.2%), Space: 18.9 MB (7.13%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -0,0 +1,34 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        visit = [[False] * n for _ in range(n)]
+        minH = maxH = grid[0][0]
+        for row in range(n):
+            maxH = max(maxH, max(grid[row]))
+            minH = min(minH, min(grid[row]))
+
+        def dfs(node, t):
+            r, c = node
+            if (min(r, c) < 0 or max(r, c) >= n or
+                visit[r][c] or grid[r][c] > t):
+                return False
+            if r == (n - 1) and c == (n - 1):
+                return True
+            visit[r][c] = True
+            return (dfs((r + 1, c), t) or
+                    dfs((r - 1, c), t) or
+                    dfs((r, c + 1), t) or
+                    dfs((r, c - 1), t))
+
+        l, r = minH, maxH
+        while l < r:
+            m = (l + r) >> 1
+            if dfs((0, 0), m):
+                r = m
+            else:
+                l = m + 1
+            for row in range(n):
+                for col in range(n):
+                    visit[row][col] = False
+
+        return r
