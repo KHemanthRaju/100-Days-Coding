f2e6db214baa3ec7cc428866d4e0127e98fbb136
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-15 08:48:56 -0700

    Time: 16 ms (8.59%), Space: 52.6 MB (6.99%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> set = new HashSet<>();
+        HashSet<String> seen = new HashSet<>();
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='.') continue;
-                String st = "("+board[i][j]+")";
-                if(!set.add(i+st) || !set.add(st+j) || !set.add(i/3+st+j/3)) return false;
+                if(board[i][j]=='.'){
+                    continue;
+                }
+                String str = "("+board[i][j]+")";
+                String rowCheck = i+str;
+                String colCheck = str+j;
+                String boxCheck = i/3+str+j/3;
+                if(seen.contains(rowCheck) || seen.contains(colCheck) || seen.contains(boxCheck)){
+                    return false;
+                }
+                seen.add(rowCheck);
+                seen.add(colCheck);
+                seen.add(boxCheck);
             }
         }
         return true;
