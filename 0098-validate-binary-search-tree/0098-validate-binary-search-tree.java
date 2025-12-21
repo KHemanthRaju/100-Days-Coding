9efe9a7b36400769a65861ec6225c725b21f2745
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-21 07:38:54 -0700

    Time: 2 ms (12.84%), Space: 45.8 MB (5.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -19,22 +19,24 @@ class Solution {
             return true;
         }
         ArrayList<Integer> ans = new ArrayList<>();
-        helper(root,ans);
-        int prev = ans.get(0);
+        helper(root, ans);
         boolean isBST = true;
+        int prev = ans.get(0);
         for(int i=1;i<ans.size();i++){
             if(ans.get(i)<=prev){
                 isBST = false;
             }
-            prev = ans.get(i);
+            prev= ans.get(i);
         }
         return isBST;
+
     }
 
     public void helper(TreeNode root, ArrayList<Integer> ans){
         if(root==null) return;
-        helper(root.left, ans);
+        helper(root.left,ans);
         ans.add(root.val);
         helper(root.right, ans);
     }
+
 }
