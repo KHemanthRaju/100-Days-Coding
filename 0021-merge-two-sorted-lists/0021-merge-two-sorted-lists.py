9099506246ccb38c2b63f226e5d77ee6a3eef3c3
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-21 05:10:43 -0700

    Time: 3 ms (12.7%), Space: 17.5 MB (96.53%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,21 +4,30 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        ans = ListNode(0)
-        curr = ans
-        while l1!=None and l2!=None:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        l1 = list1
+        l2 = list2
+        head = ListNode(0)
+        dummy = head
+        if l1==None and l2==None:
+            return None
+        if l1==None:
+            return l2
+        if l2 == None:
+            return l1
+        while l1 and l2:
             if l1.val < l2.val:
-                curr.next = l1
+                dummy.next = l1
                 l1 = l1.next
             else:
-                curr.next = l2
+                dummy.next = l2
                 l2 = l2.next
-            curr = curr.next
+            dummy = dummy.next
         if l1!=None:
-            curr.next = l1
-            l1=l1.next
-        elif l2!=None:
-            curr.next = l2
+            dummy.next = l1
+            l1 = l1.next
+        if l2!=None:
+            dummy.next = l2
             l2 = l2.next
-        return ans.next
+        return head.next
