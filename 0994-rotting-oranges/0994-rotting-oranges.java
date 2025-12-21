6e8b9b92f8e9d85f7fa6de741d208e5626116699
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-21 13:12:05 -0700

    Time: 2 ms (86.06%), Space: 44.2 MB (45.27%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,14 +1,12 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid == null|| grid.length == 0 || grid[0].length==0){
-            return 0;
-        }
-        int ro = grid.length;
-        int co = grid[0].length;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int time = 0;
         Queue<int[]> q = new LinkedList<>();
         int fresh = 0;
-        for(int i=0;i<ro;i++){
-            for(int j=0;j<co;j++){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 if(grid[i][j]==1){
                     fresh++;
                 }
@@ -17,22 +15,21 @@ class Solution {
                 }
             }
         }
-        int time = 0;
-        int[][] dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+        int[][] directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         while(!q.isEmpty()){
-            int[] ans = q.poll();
-            int r = ans[0], c = ans[1], t=ans[2];
+            int ans[] = q.poll();
+            int r = ans[0], c = ans[1], t = ans[2];
             time = t;
-            for(int[] d:dir){
-                int row = d[0]+r;
-                int col = d[1]+c;
-                if(0<=row && row<grid.length && 0<=col && col<grid[0].length && grid[row][col]==1){
+            for(int[] dir:directions){
+                int row = r + dir[0];
+                int col = c + dir[1];
+                if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && grid[row][col]==1){
                     fresh--;
-                    grid[row][col] = 2;
-                    q.add(new int[]{row, col, t+1});
+                    grid[row][col]=2;
+                    q.offer(new int[]{row,col,t+1});
                 }
             }
         }
-        return fresh == 0? time : -1;
+        return fresh==0?time:-1;
     }
 }
