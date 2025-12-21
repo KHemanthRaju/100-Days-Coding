a5572c1e378f0fe1c14554fba73cbbc8afc02c94
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-21 10:01:26 -0700

    Time: 23 ms (26.34%), Space: 21 MB (42.47%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        delta=defaultdict(int)
+        for src, dest in trust:
+            delta[src] -= 1
+            delta[dest] += 1
+        for i in range(1, n+1):
+            if delta[i] == n- 1:
+                return i
+        return -1
