7ad8573787428b7324abe7998717de809ba4d16e
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-25 19:51:44 -0700

    Time: 2 ms (39.72%), Space: 17.6 MB (93.87%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,17 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        used =[False] * len(nums)
+        ans = []
+        used = [False] * len(nums)
         def backtrack(path):
-            if len(nums) == len(path):
-                result.append(path[:])
+            if len(path) == len(nums):
+                ans.append(path[:])
                 return
             for i in range(len(nums)):
-                if used[i] == False:
+                if used[i]==False:
                     used[i] = True
                     path.append(nums[i])
                     backtrack(path)
                     path.pop()
                     used[i] = False
+
         backtrack([])
-        return result
+        return ans
