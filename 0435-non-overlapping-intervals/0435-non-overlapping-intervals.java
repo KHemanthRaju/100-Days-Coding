20572cdebf75ac9722a2f74baae1aec6ed9b22b6
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-26 12:00:53 -0700

    Time: 46 ms (71.64%), Space: 115.8 MB (49.65%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -3,15 +3,14 @@ class Solution {
         Arrays.sort(intervals, (a,b)->a[1]-b[1]);
         int count = 0;
         int prev = intervals[0][1];
-        for(int i=1;i<intervals.length;i++){
+        for(int i = 1;i<intervals.length;i++){
             int start = intervals[i][0];
             int end = intervals[i][1];
             if(start<prev){
-                count += 1;
+                count+=1;
             }else{
                 prev = end;
             }
-            
         }
         return count;
     }
