fc5fe0855abef2e9b39ba6d9d9767bca2d4d46c4
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-25 19:19:25 -0700

    Time: 0 ms (100%), Space: 17.6 MB (92.45%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -5,7 +5,8 @@ class Solution:
             ans.append(path[:])
             for i in range(start, len(nums)):
                 path.append(nums[i])
-                backtrack(i+1,path)
+                backtrack(i+1, path)
                 path.pop()
         backtrack(0,[])
         return ans
+        
