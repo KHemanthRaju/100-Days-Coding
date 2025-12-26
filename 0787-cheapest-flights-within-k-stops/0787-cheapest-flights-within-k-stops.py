47997b15dc1b665db7c562d9b4153e1e68660f2c
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-26 12:40:09 -0700

    Time: 91 ms (23.49%), Space: 18.4 MB (97.1%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(k+1):
             tmpPrices = prices.copy()
             for s,d,p in flights:
-                if prices[s] == float("inf"):
+                if prices[s]==float("inf"):
                     continue
                 if prices[s]+p < tmpPrices[d]:
                     tmpPrices[d] = prices[s]+p
