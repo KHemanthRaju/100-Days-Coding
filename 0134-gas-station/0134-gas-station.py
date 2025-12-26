5d6a23c12149301ab310f0e9b847813b372547f9
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-25 21:06:14 -0700

    Time: 9 ms (99.8%), Space: 24.1 MB (12.51%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,17 +1,12 @@
-class Solution(object):
-    def canCompleteCircuit(self, gas, cost):
-        """
-        :type gas: List[int]
-        :type cost: List[int]
-        :rtype: int
-        """
-        if sum(gas) < sum(cost):
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(cost)>sum(gas):
             return -1
+        tank =0
         start = 0
-        tank = 0
         for i in range(len(gas)):
-            tank += gas[i] - cost[i]
-            if tank<0:
-                start=i+1
+            tank += gas[i]-cost[i]
+            if tank <0:
+                start = i+ 1
                 tank = 0
         return start
