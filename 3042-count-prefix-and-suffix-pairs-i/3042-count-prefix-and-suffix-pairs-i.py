000a6b72f67526d44df751b175ead1448661c258
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-25 17:14:37 -0700

    Time: 11 ms (61.6%), Space: 17.6 MB (87.91%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
@@ -3,10 +3,9 @@ class Solution:
         n = len(words)
         count = 0
         for i in range(n):
-            for j in range(i+1,n):
+            for j in range(i+1, n):
                 str1 = words[i]
                 str2 = words[j]
-
                 if len(str1) > len(str2):
                     continue
                 if str2.startswith(str1) and str2.endswith(str1):
