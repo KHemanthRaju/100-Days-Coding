1bed97ce7b138a532d0d6c5d10392ac0d4cccb6f
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2025-12-28 04:46:30 -0700

    Time: 2403 ms (29.96%), Space: 178 MB (15.37%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -5,20 +5,21 @@ class Solution:
         for i in range(N):
             x1,y1 = points[i]
             for j in range(i+1, N):
-                x2,y2 = points[j]
+                x2, y2 = points[j]
                 dist = abs(x1-x2)+abs(y1-y2)
-                adj[i].append([dist, j])
+                adj[i].append([dist,j])
                 adj[j].append([dist, i])
-        visit = set()
+        visit= set()
+        heap = [[0,0]]
         res = 0
-        minH = [[0,0]]
         while len(visit)<N:
-            cost, i = heapq.heappop(minH)
+            cost, i = heapq.heappop(heap)
             if i in visit:
                 continue
-            res += cost
+            res+=cost
             visit.add(i)
             for neiCost, nei in adj[i]:
                 if nei not in visit:
-                    heapq.heappush(minH, [neiCost, nei])
+                    heapq.heappush(heap, [neiCost, nei])
         return res
+
