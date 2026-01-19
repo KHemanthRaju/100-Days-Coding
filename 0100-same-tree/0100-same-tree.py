f3113b47c500ed0e9d22e898e2de500a6edca5b2
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-01-19 10:36:59 -0700

    Time: 0 ms (100%), Space: 19.3 MB (16.93%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,36 +4,11 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-"""
-Understand - Input - 2 trees, Output - boolean,  edge cases - 2 trees are empty-return true, 1 tree is empty, another tree has node - return false
-
-Plan - root1 and root2 is null, iterative, queue - [p.val, q.val] , if p.val==q.val, p.val != q.val, return false, [p.left, q.left], [p.right, q.right]
-"""
-
-from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # queue = deque([(p,q)])
-
-        # while queue:
-        #     node1, node2 = queue.popleft()
-
-        #     if not node1 and not node2:
-        #         continue
-
-        #     if not node1 or not node2 or node1.val != node2.val:
-        #         return False
-            
-        #     queue.append((node1.left, node2.left))
-        #     queue.append((node1.right, node2.right))
-        
-        # return True
         if not p and not q:
             return True
         if not p or not q or p.val!=q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-
-    # ///Time complexity (O(min(m,n))
-    # //Space Complexity : (O(min(m,n))
         
