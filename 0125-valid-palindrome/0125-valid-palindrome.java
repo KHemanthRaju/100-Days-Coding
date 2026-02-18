0db6f21d22cd73168ab875abb00d06e3fefbfe9f
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-02-18 01:03:12 -0700

    Time: 2 ms (98.99%), Space: 44.5 MB (67.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0;
-        int r = s.length()-1;
-        while(l<=r){
-            while(l<r && !check(s.charAt(l))){
-                l++;
+        if(s.length()==0){
+            return true;
+        }
+        int left = 0;
+        int right = s.length()-1;
+        while(left<=right){
+            while(left < right && !isAlphaNum(s.charAt(left))){
+                left++;
+            }while(left < right && !isAlphaNum(s.charAt(right))){
+                right--;
             }
-            while(l<r && !check(s.charAt(r))){
-                r--;
-            }
-            if(Character.toLowerCase(s.charAt(l))!= Character.toLowerCase(s.charAt(r))){
+            if(Character.toLowerCase(s.charAt(left))!= Character.toLowerCase(s.charAt(right))){
                 return false;
             }
-            l++;
-            r--;
+            left++;
+            right--;
         }
         return true;
     }
-
-    public boolean check(Character ch){
-        if(ch>='a' && ch<='z' || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')){
+    public boolean isAlphaNum(Character ch){
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch >= '0' && ch <='9')){
             return true;
         }
         return false;
     }
+    
 }
