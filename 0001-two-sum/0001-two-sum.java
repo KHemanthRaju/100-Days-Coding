d1639b131e828ceb64f6b51b6aba3a35f79a527a
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-02-17 23:35:35 -0700

    Time: 2 ms (99.18%), Space: 47.2 MB (56.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        if(nums==null) throw new IllegalArgumentException("the nums is null");
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(target-nums[i])){
