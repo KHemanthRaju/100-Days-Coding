6b89f65cc185b45a3b4371719f81b12483d21e0a
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-02-18 19:07:07 -0700

    Time: 5 ms (58.81%), Space: 44.7 MB (61.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,13 +3,13 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int[] hashTableS = new int[26];
+        int[] ans = new int[26];
         for(int i=0;i<s.length();i++){
-            hashTableS[s.charAt(i)-'a']++;
-            hashTableS[t.charAt(i)-'a']--;
+            ans[s.charAt(i)-'a']++;
+            ans[t.charAt(i)-'a']--;
         }
-        for(int i=0;i<hashTableS.length;i++){
-            if(hashTableS[i]!=0){
+        for(int i=0;i<ans.length;i++){
+            if(ans[i]!=0){
                 return false;
             }
         }
