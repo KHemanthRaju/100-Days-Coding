3bdc2bd23f74fc19d77cf3709f4b679394b7ff63
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-03-18 19:54:48 -0600

    Time: 6 ms (69.52%), Space: 46.1 MB (59.56%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,11 +5,12 @@ class Solution {
         HashSet<Character> seen = new HashSet<>();
         for(int endWindow=0;endWindow<s.length();endWindow++){
             while(seen.contains(s.charAt(endWindow))){
+                
                 seen.remove(s.charAt(startWindow));
                 startWindow++;
             }
             seen.add(s.charAt(endWindow));
-            longest = Math.max(longest, endWindow-startWindow+1);
+            longest = Math.max(longest, endWindow - startWindow + 1);
         }
         return longest;
     }
