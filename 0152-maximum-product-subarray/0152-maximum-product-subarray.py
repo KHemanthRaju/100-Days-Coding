5cce58f58b2f9e7f2288e2c8db0fb22b17e99641
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-03-22 16:50:27 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -2,7 +2,7 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        currMax,currMin = 1,1
+        currMin, currMax = 1,1
         res = max(nums)
         for num in nums:
             if num == 0:
@@ -10,6 +10,6 @@ class Solution:
                 continue
             temp = num * currMax
             currMax = max(num*currMax, num*currMin, num)
-            currMin = min(num*currMin, temp, num)
+            currMin = min(num*currMax, temp, num)
             res = max(res, currMax)
         return res
