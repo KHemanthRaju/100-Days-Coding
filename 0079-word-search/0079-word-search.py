51540eb6b319ad807fb43a3b8d5d797b3bd21b4c
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-03-26 11:29:07 -0600

    Time: 3371 ms (81.67%), Space: 19.4 MB (73.55%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -5,17 +5,16 @@ class Solution:
         def backtrack(r,c,index):
             if index == len(word):
                 return True
-            if r<0 or c<0 or r>=ROWS or c>=COLS or board[r][c] != word[index]:
+            if r<0 or r>= ROWS or c<0 or c >= COLS or board[r][c]!=word[index]:
                 return False
             temp = board[r][c]
             board[r][c] = "#"
-            found = (backtrack(r+1,c,index+1) or backtrack(r-1,c,index+1) or backtrack(r, c+1, index+1) or backtrack(r,c-1,index+1))
+            found = (backtrack(r+1,c,index+1) or backtrack(r-1, c, index+1) or backtrack(r, c+1, index+1) or backtrack(r, c-1, index+1))
             board[r][c] = temp
             return found
-            
         for i in range(ROWS):
             for j in range(COLS):
-                if board[i][j] == word[0]:
+                if board[i][j]==word[0][0]:
                     if backtrack(i,j,0):
                         return True
         return False
