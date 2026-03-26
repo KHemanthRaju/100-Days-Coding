d7ccb777004b0f73f910f8ccc249ff4cd41a573f
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-03-26 10:33:18 -0600

    Time: 0 ms (100%), Space: 43.8 MB (64.56%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,33 +1,26 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int l = 0;
-        int r = matrix.length - 1;
-        
-        while ( l < r )
-        {
-            for(int i = 0; i < r - l; i++)
-            {
-                int top = l;
-                int bottom = r;
-                 //save the topleft
-                int topLeft = matrix[top][l + i];
-
-                //move bottom left into top left
-                matrix[top][l + i] = matrix[bottom - i][l];
-
-                // move bottom right into bottom left
-                matrix[bottom - i][l] = matrix[bottom][r - i];
-
-                // move top right into bottom right
-                matrix[bottom][r - i] = matrix[top + i][r];
-
-                // move top left into top right
-                matrix[top + i][r] = topLeft;
-                
+        //transpose and reverse it
+        int n = matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
-            
-            r -= 1;
-            l += 1;
+        }
+
+        //reverse the matrix
+        for(int i=0;i<n;i++){
+            int left = 0;
+            int right = n-1;
+            while(left<right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
         }
     }
 }
