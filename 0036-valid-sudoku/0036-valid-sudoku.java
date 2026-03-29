3cc61d40b3a3198b11c1f149dc473955d9dfc193
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-03-28 23:05:47 -0600

    Time: 20 ms (7.59%), Space: 52.5 MB (11.84%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -3,13 +3,13 @@ class Solution {
         HashSet<String> seen = new HashSet<>();
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='.'){
+                if(board[i][j] == '.'){
                     continue;
                 }
                 String point = "("+board[i][j]+")";
-                String row = i+point;
-                String col = point+j;
-                String mat = i/3+point+j/3;
+                String row = i + point;
+                String col = point + j;
+                String mat = i/3 + point + j/3;
                 if(seen.contains(row) || seen.contains(col) || seen.contains(mat)){
                     return false;
                 }
