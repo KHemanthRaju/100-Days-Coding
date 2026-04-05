b707c12f26bce4eff5ad826be186c3f2ae21240f
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-04-05 12:05:34 -0600

    Time: 167 ms (5.02%), Space: 46.7 MB (70.58%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,46 +10,43 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0){
+        if(lists.length==0){
             return null;
         }
-        if(lists.length == 1){
+        if(lists.length==1){
             return lists[0];
         }
-        ListNode newList = mergeTwoLinkedLists(lists[0],lists[1]);
+        ListNode newList = mergeTwoLists(lists[0], lists[1]);
         for(int i=2;i<lists.length;i++){
-            newList = mergeTwoLinkedLists(newList, lists[i]);
+            newList = mergeTwoLists(newList, lists[i]);
         }
-
         return newList;
     }
-
-    public ListNode mergeTwoLinkedLists(ListNode l1, ListNode l2){
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
         ListNode ans = new ListNode(0);
         ListNode head = ans;
-        while(l1!=null && l2!=null){
-            if(l1.val<l2.val){
-                ans.next = l1;
-                l1 = l1.next;
-            }else{
-                ans.next = l2;
-                l2 = l2.next;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                ans.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                ans.next = list2;
+                list2 = list2.next;
             }
             ans = ans.next;
         }
-
-        while(l1!=null){
-            ans.next = l1;
-            l1 = l1.next;
-            ans = ans.next;
+        while(list1!=null){
+            ans.next = list1;
+            ans=ans.next;
+            list1 = list1.next;
         }
 
-        while(l2!=null){
-            ans.next = l2;
-            l2 = l2.next;
+        while(list2!=null){
+            ans.next = list2;
             ans = ans.next;
+            list2 = list2.next;
         }
-
         return head.next;
     }
 }
