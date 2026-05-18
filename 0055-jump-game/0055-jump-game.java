59d6398a579be75145ac4330d7df19cd404fe66e
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-05-18 12:17:35 -0700

    Time: 2 ms (87.77%), Space: 48 MB (41.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int far = 0;
+        int farthest = 0;
         for(int i=0;i<nums.length;i++){
-            if(i>far) return false;
-            far = Math.max(far,i+nums[i]);
+            if(i > farthest){
+                return false;
+            }
+            farthest = Math.max(farthest, i + nums[i]);
         }
         return true;
     }
