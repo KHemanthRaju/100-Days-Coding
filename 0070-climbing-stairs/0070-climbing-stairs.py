f346f13f110e649b834051375adc9d377449e3b6
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2026-05-20 17:15:49 -0700

    Time: 0 ms (100%), Space: 19.4 MB (16.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,12 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if(n==0):
-            return 0
-        if n==1:
-            return 1
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==1 or n==0:
+            return n
+        if n==2:
+            return 2
         dp = [0]*(n+1)
-        dp[0] = 0
         dp[1] = 1
         dp[2] = 2
         for i in range(3, n+1):
-            dp[i] = dp[i-1]+dp[i-2]
+            dp[i] = dp[i-1] + dp[i-2]
         return dp[n]
